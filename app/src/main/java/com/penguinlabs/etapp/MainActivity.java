a4e9d87e26e892144b8d97890fdc07a11e376ccb
exploremaruf <cyberistmaruf@gmail.com>
2026-02-07 23:20:56 +0600

    Add Product model class

--- app/src/main/java/com/penguinlabs/etapp/MainActivity.java
+++ app/src/main/java/com/penguinlabs/etapp/MainActivity.java
@@ -71,4 +71,23 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {}
         });
     }
+    public class Product {
+
+        private String name;
+        private String price;
+
+        public Product(String name, String price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+    }
+
 }
